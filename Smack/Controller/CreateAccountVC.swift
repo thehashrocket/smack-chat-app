8e8ad9b891f2cd9e5f33d8a37260dba5e49d3327
YOUR NAME <jasshultz@gmail.com>
2017-10-17 18:40:29 -0600

    lesson 10

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    @IBOutlet weak var passTxt: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,10 +29,25 @@ class CreateAccountVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func createAcctPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
-    
-
 
 }
