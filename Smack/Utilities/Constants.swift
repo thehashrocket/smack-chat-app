4b48df2eaeb7a669b3a1efec36fbe2a4d1d1ae31
YOUR NAME <jasshultz@gmail.com>
2017-10-17 18:20:56 -0600

    lesson 09

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-// Segues
+typealias CompleteHandler = (_ Success: Bool) -> ()
 
+// USR Constants
+let BASE_URL = "https://intense-gorge-67117.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
