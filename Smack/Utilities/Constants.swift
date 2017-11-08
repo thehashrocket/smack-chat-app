d6936323fa9311976e4dac7c5fa7aa00e3ab150b
YOUR NAME <jasshultz@gmail.com>
2017-11-07 19:14:46 -0700

    logging in user

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompleteHandler = (_ Success: Bool) -> ()
 // USR Constants
 let BASE_URL = "https://intense-gorge-67117.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -23,3 +24,8 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset = utf-8"
+]
