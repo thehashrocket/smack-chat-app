c8d739d3b3dceb0750a9d4d388cae19ca1f1ad97
YOUR NAME <jasshultz@gmail.com>
2017-11-19 21:54:52 -0700

    did things. not sure what. forgot

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -31,7 +31,14 @@ class ChannelVC: UIViewController {
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn {
+            // show profile page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
     }
     
     @objc func userDataDidChange(_ notif: Notification) {
---
+++ Smack/Controller/ProfileVC.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by Jason Shultz on 11/17/17.
+//  Copyright © 2017 Jason Shultz. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userEmail: UILabel!
+    
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        profileImage.image = UIImage.init(named: UserDataService.instance.avatarName)
+        profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
